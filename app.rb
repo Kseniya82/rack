ee30f6364aa321337058c76298351a2eecf36872
Kseniya82 <44781744+Kseniya82@users.noreply.github.com>
2019-03-20 11:15:57 +0300

    Rack interface revision 3

---
+++ app.rb
@@ -0,0 +1,32 @@
+require_relative 'time_format'
+class App
+  def call(env)
+    request = Rack::Request.new(env)
+    return bad_request if request.path != '/time'
+
+    time_format = TimeFormat.new(request.params['format'].split(','))
+    if time_format.bad_time_format?
+      bad_format(time_format.unknown_format)
+    else
+      rigth_format(time_format.set_time)
+    end
+  end
+
+  private
+
+  def bad_request
+    make_response(404, 'Path not foumd')
+  end
+
+  def bad_format(format)
+    make_response(400, "Unknown time format #{format}")
+  end
+
+  def rigth_format(format)
+    make_response(200, format)
+  end
+
+  def make_response(status, headers = { 'content-type' => 'text-plain' }, body)
+    [status, headers, ["#{body}\n"]]
+  end
+end
---
+++ time_format.rb
@@ -0,0 +1,34 @@
+class TimeFormat
+  FORMATS = {
+    'year' => '%Y',
+    'month' => '%m',
+    'day' => '%d',
+    'hour' => '%H',
+    'minute' => '%M',
+    'second' => '%H'
+  }.freeze
+
+  attr_reader :unknown_format
+
+  def initialize(request_format)
+    time_format(request_format)
+  end
+
+  def set_time
+    Time.now.strftime(format_time) unless @format_time.empty?
+  end
+
+  def bad_time_format?
+    !@unknown_format.empty?
+  end
+
+  private
+
+  def format_time
+    @format_time.map { |format| FORMATS[format] }.join("-")
+  end
+
+  def time_format(request_format)
+    @format_time, @unknown_format = request_format.partition { |format| FORMATS.include?(format)}
+  end
+end
